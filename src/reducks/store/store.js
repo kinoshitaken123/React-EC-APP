54820d80b0d1417fe113691a7429d88e713f0b9c
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-21 21:45:44 +0900

    [Middleware導入]

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,14 +1,18 @@
 import{
     createStore as reduxCreateStore,
-    combineReducers
+    combineReducers,
+    applyMiddleware　//redux package
 } from "redux";
+import {UsersReducer, roterMiddleware, connectRouter} from "connected-react-router";
 
-import {UsersReducer} from "../users/reducers";
-
-export default function createStore () {
+export default function createStore (history) {  //history 今どのパスいるかを表す
     return reduxCreateStore(
         combineReducers({
+            router: connectRouter(history),
             users: UsersReducer
-        })
-    )
+        }),
+        applyMiddleware(
+            roterMiddleware(history)
+        )
+    );
 }
