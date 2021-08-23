492ed2c015d9608e619d59f5fcdd42c5f84b2908
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-23 11:41:31 +0900

    [selectorsファイル作成]

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,17 @@
 // URLの遷移を確認するページ
+import {getUserId} from "../reducks/users/selectors";
+import {useSelector} from 'react-redux';
 
 const Home = () => {
+    const selector = useSelector( state => state);
+    const uid = getUserId(selector)
+
     return (
-        <h2>Home</h2>
+        <div>
+          <h2>Home</h2>
+          <p>{uid}</p>
+        </div>
+
     )
 }
 
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {push} from 'connected-react-router';
+import {signInAction} from "../reducks/users/actions";
 
 const Login = () => {
     const dispatch = useDispatch()
-    const selector = useSelector(state => state);
-
-    console.log(selector.router)
+    
     return(
         <div>
             <h2>ログイン</h2>
-            <button onClick={() => dispatch(push('/'))}>
+            <button onClick={() => {
+                 dispatch(signInAction({uid: "00001",username: "ken"}));
+                 dispatch(push('/'))
+             }}>
                 ログインする
             </button>
         </div>
