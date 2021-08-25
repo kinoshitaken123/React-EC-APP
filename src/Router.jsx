0747a8e016bac482c0fd1760fd3794339b63cc1b
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-25 11:11:34 +0900

    [signup form]

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
-import { LoginContainer } from './containers'
-import {Login, Home} from'./templates'
+import {Login, Home,SignUp} from'./templates'
 
 const Router = () => {
     return (
         <Switch>
-            <Route exact path={"/login"} component={LoginContainer} />
+            <Route exact path={"/signup"} component={SignUp} />
+            <Route exact path={"/login"} component={Login} />
             <Route exact path={"(/)?"} component={Home} />     {/* スラッシュがあってもなくてもHOME画面を表示させる */}
         </Switch>
     )
---
+++ src/templates/SignUp.jsx
@@ -0,0 +1,57 @@
+import React,{useState,useCallback} from 'react';
+import {TextInput} from '../components/UIkit';
+
+const SignUp = () =>{
+    
+    const [username, setUsername] = useState(""), //const宣言　複数行可能
+          [email, setEmail] = useState(""),
+          [password, setPassword] = useState(""),
+          [confirmPassword,setConfirmPassword] = useState("");
+
+    const inputUsername = useCallback((event)=>{
+        setUsername(event.target.value)
+
+    },[setUsername]);    
+    
+    const inputEmail = useCallback((event)=>{
+        setEmail(event.target.value)
+
+    },[setEmail]);     
+
+    const inputPassword = useCallback((event)=>{
+        setPassword(event.target.value)
+
+    },[setPassword]);     
+
+    const inputConfirmPassword  = useCallback((event)=>{
+        setConfirmPassword(event.target.value)
+
+    },[setConfirmPassword]);     
+
+    return (
+        <div className="c-section-container">
+            <h2 className="u-text_headliine u-text-center">アカウント登録</h2>
+            <div className="module-spacer--medium" />
+            <TextInput 
+             fullwidth={true} label={"ユーザー名"} multiline={false} required={true}
+             rows={1} value={username} type={"text"} onChange={inputUsername}
+             />
+            <TextInput 
+             fullwidth={true} label={"メールアドレス"} multiline={false} required={true}
+             rows={1} value={email} type={"email"} onChange={inputEmail}
+             />
+            <TextInput 
+             fullwidth={true} label={"パスワード(半角英数字で６文字以上)"} multiline={false} required={true}
+             rows={1} value={password} type={"password"} onChange={inputPassword}
+             />
+            <TextInput 
+             fullwidth={true} label={"パスワードの再確認"} multiline={false} required={true}
+             rows={1} value={confirmPassword} type={"password"} onChange={inputConfirmPassword}
+             />
+
+        </div>
+    )
+    
+}
+
+export default SignUp
