e317d77af4e9b01c86944f2de5f5add1e594d869
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-30 19:28:34 +0900

    [Drawer]

--- src/components/Header/ClosableDrawer.jsx
+++ src/components/Header/ClosableDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -13,6 +13,9 @@ import HistoryIcon from '@material-ui/icons/History';
 import PersonIcon from '@material-ui/icons/Person';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import {TextInput} from "../UIkit/index";
+import {push} from 'connected-react-router';
+import {useDispatch, useSelector} from "react-redux";
+import{signOut} from "../../reducks/users/operations";
 
 const useStyles = makeStyles((theme) => ({
     drawer: {
@@ -37,12 +40,24 @@ const useStyles = makeStyles((theme) => ({
 const ClosableDrawer = (props) => {
     const classes = useStyles();
     const {container} = props;
+    const dispatch = useDispatch();
 
     const [keyword, setKeyword] = useState("");
 
     const inputKeyword = useCallback((event) => {
         setKeyword(event.target.value)
     }, [setKeyword]);
+
+    const selectMenu = (event, path) => {
+        dispatch(push(path));
+        props.onClose(event);
+    };
+
+    const menus = [
+        {func: selectMenu, label: "商品登録",    icon: <AddCircleIcon/>, id: "register", value: "/product/edit"},
+        {func: selectMenu, label: "注文履歴",    icon: <HistoryIcon/>,   id: "history",  value: "/order/history"},
+        {func: selectMenu, label: "プロフィール", icon: <PersonIcon/>,    id: "profile",  value: "/user/mypage"},
+    ]
 
     return (
         <nav className={classes.drawer}>
@@ -67,7 +82,17 @@ const ClosableDrawer = (props) => {
                 </div>   
                 <Divider /> 
                 <List>
-                   <ListItem button key="logout">
+                    {menus.map(menu => (
+                        // ((isAdministrator && menu.id === "register") || menu.id !== "register") && (
+                            <ListItem button key={menu.id} onClick={(event) => menu.func(event, menu.value)}>
+                                <ListItemIcon>
+                                    {menu.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={menu.label} />
+                            </ListItem>
+                            // )
+                        ))}
+                    <ListItem button key="logout" onClick={() => dispatch(signOut())}>
                        <ListItemIcon>
                           <ExitToAppIcon/>
                        </ListItemIcon>
